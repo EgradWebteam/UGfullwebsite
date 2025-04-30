81605e6f55cbcca934ac0cfdb45e761fd976fa31
sindhu-121 <sindhu.g@egradtutor.in>
2025-04-30 10:37:09 +0530

    docker

--- ugonlineportalbackend/server.js
+++ ugonlineportalbackend/server.js
@@ -7,6 +7,22 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+connection.connect(err => {
+  if (err) {
+    console.error('Database connection failed:', err.stack);
+    return;
+  }
+  console.log('Connected to MySQL database');
+});
 
 app.get('/', (req, res) => {
   res.send('API is running...');
